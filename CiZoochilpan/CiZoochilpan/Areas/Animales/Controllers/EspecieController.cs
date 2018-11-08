fcae8efdd10e9ebb68f83fa229b9c0d983a4d710
Tanatos skil's <Tanathos1@hotmail.com>
2018-11-08 17:48:28 -0600

    evento cambio de clase

---
+++ CiZoochilpan/CiZoochilpan/Areas/Animales/Controllers/EspecieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CiZoochilpan.Areas.Animales.Controllers
+{
+    public class EspecieController : Controller
+    {
+        // GET: Animales/Especie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CiZoochilpan/CiZoochilpan/Areas/Animales/Controllers/FamiliaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CiZoochilpan.Areas.Animales.Controllers
+{
+    public class FamiliaController : Controller
+    {
+        // GET: Animales/Familia
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
