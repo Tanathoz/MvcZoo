ea5c115127613bb59bdc232d5699b8622c4f0272
Tanatos skil's <Tanathos1@hotmail.com>
2018-12-07 18:24:43 -0600

    controller dieta ejemplar

---
+++ CiZoochilpan/CiZoochilpan/Areas/Dietas/Controllers/DietaEjemplarController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using CiZoochilpan.Areas.Dietas.Models;
+namespace CiZoochilpan.Areas.Dietas.Controllers
+{
+    public class DietaEjemplarController : Controller
+    {
+
+        // GET: Dietas/DietaEjemplar
+        public async Task<ActionResult> Index(string textoBuscar)
+        {
+            IEnumerable<DietaEjemplarModelView> lstDietas = null;
+            using (var client = new HttpClient())
+            {
+                if (textoBuscar ==null || textoBuscar == string.Empty)
+                {
+                    client.BaseAddress = new Uri("http://tanathoz-001-site1.ctempurl.com/api/");
+                    var responseTask = client.GetAsync("DietaEjemplar");
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<IList<DietaEjemplarModelView>>();
+                        readTask.Wait();
+                        lstDietas = readTask.Result;
+                    }else
+                    {
+                        lstDietas = Enumerable.Empty<DietaEjemplarModelView>();
+                        ModelState.AddModelError(string.Empty, "Error al obtener datos de la dieta");
+
+                    }
+                }else
+                {
+                    client.BaseAddress = new Uri("http://tanathoz-001-site1.ctempurl.com/api/");
+                    var responseTask = client.GetAsync("DietaEjemplar?valor=" + textoBuscar);
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<IList<DietaEjemplarModelView>>();
+                        readTask.Wait();
+                        lstDietas = readTask.Result;
+                    }
+                    else
+                    {
+                        lstDietas = Enumerable.Empty<DietaEjemplarModelView>();
+                        ModelState.AddModelError(string.Empty, "Error al obtener datos de la dieta");
+                    }
+                }
+            }
+                return View(lstDietas);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(DietaEjemplarModelView dieta)
+        {
+            using (var client  = new HttpClient()){
+                client.BaseAddress = new Uri("http://tanathoz-001-site1.ctempurl.com/api/DietaEjemplar");
+                var postTask = client.PostAsJsonAsync<DietaEjemplarModelView>("DietaEjemplar",dieta);
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    TempData["Success"] = "Se ha registrado una dieta de ejemplar exitosamente";
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError(string.Empty, "Error con el servidor al intentar guardar");
+                TempData["Error"] = "Error al intentar guardar ";
+                return RedirectToAction("Index");
+
+
+            }
+        }
+        public ActionResult Edit(string marcaje)
+        {
+            DietaEjemplarModelView dieta = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://tanathoz-001-site1.ctempurl.com/api/");
+                var responseTask = client.GetAsync("DietaEjemplar?marcaje=" + marcaje.ToString());
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<DietaEjemplarModelView>();
+                    readTask.Wait();
+                    dieta = readTask.Result;
+
+                }
+            }
+            return View(dieta);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DietaEjemplarModelView dieta)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://tanathoz-001-site1.ctempurl.com/api/DietaEjemplar");
+                var putTask = client.PutAsJsonAsync<DietaEjemplarModelView>("DietaEjemplar", dieta);
+                putTask.Wait();
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    TempData["Success"] = "Se ha guardado una dieta de ejemplar exitosamente";
+                    return RedirectToAction("Index");
+                }
+                TempData["Error"] = "Ha ocurrido un error al editor";
+                return RedirectToAction("Index");
+
+            }
+        }
+
+        public ActionResult Delete( string marcaje)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://tanathoz-001-site1.ctempurl.com/api/");
+                var deleteTask = client.DeleteAsync("DietaEjemplar/?marcaje="+marcaje);
+                deleteTask.Wait();
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    TempData["Success"] = "Se ha eliminado una dieta de forma exitosa";
+                    return RedirectToAction("Index");
+                }
+            }
+
+            TempData["Error"] = "Ha ocurrido un error no es posible borrar";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CiZoochilpan/CiZoochilpan/Areas/Dietas/Models/DietaEjemplarModelView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace CiZoochilpan.Areas.Dietas.Models
+{
+    public class DietaEjemplarModelView
+    {
+        [Required(ErrorMessage = "Seleccionar ejemplar es obligatorio ")]
+        public string marcaje { get; set; }
+        public string nombreComun { get; set; }
+        public string nombrePropio { get; set; }
+        public string fechaCambio { get; set; }
+        [Required(ErrorMessage ="El campo de causa cambio es obligatorio")]
+        [MinLength (5, ErrorMessage ="La longitud minima es de 5 caracteres")]
+        [MaxLength (50, ErrorMessage ="La longituf máxima es de 50 caracteres")]
+        public string causaCambio { get; set; }
+        public string cantidad { get; set; }
+        [Required(ErrorMessage ="El campo alimento es obligatorio")]
+        [MinLength(5, ErrorMessage = "La longitud minima es de 5 caracteres")]
+        [MaxLength(50, ErrorMessage = "La longituf máxima es de 50 caracteres")]
+        public string alimento { get; set; }
+
+        public string horario { get; set; }
+        public string consideraciones { get; set;}
+
+    }
+}
